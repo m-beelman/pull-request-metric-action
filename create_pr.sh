82c1ab84e13ffb470f3d0e8369a6883142ed4be3
Marko Beelmann <62096921+m-beelman@users.noreply.github.com>
2023-02-07 23:56:56 +0100

    Created branch mbee18 (#31)

--- create_pr.sh
+++ create_pr.sh
@@ -5,7 +5,7 @@ git commit -am "Created branch $1"
 git push --set-upstream origin $1
 gh pr create --title "Created branch $1" --body "Created branch $1" --base main --head $1
 gh pr comment "$1" --body "Created branch $1"
-gh pr review $1 --comment -b "interesting"
+#gh pr review $1 --comment -b "interesting"
 gh pr merge "$1" --squash --delete-branch
 git checkout main
 git pull
