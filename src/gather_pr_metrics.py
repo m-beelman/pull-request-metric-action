f6546fa62e2133777b387bbd8d4d92c3223d18f7
Beelmann, Marko <marko.beelmann@philips.com>
2023-02-07 17:57:32 +0100

    Add action and workflow

---
+++ src/gather_pr_metrics.py
@@ -0,0 +1,128 @@
+import os
+from datetime import datetime
+import json
+import sys
+
+def convert_to_datetime(date_string: str):
+    if (date_string == None):
+        return None
+    return datetime.fromisoformat(date_string.replace('Z', '+00:00'))
+
+def get_detailed_pull_request_event_history(pr_id: int):
+    return os.popen(f'gh pr view {pr_id} --json commits,reviews,state,comments,closedAt,createdAt,mergedAt').read()
+
+def get_first_event_of_type(events_with_time: list, type: str):
+    return next((event for event in events_with_time if event['type'] == type), None)
+
+def get_last_event_of_type(events_with_time: list, type: str):
+    return next((event for event in reversed(events_with_time) if event['type'] == type), None)
+
+def get_time_spend_on_branch_before_pr_created_from_timeline(events_with_time: list):
+    first_commit = get_first_event_of_type(events_with_time, 'commit')
+    first_create = get_first_event_of_type(events_with_time, 'createAt')
+    return (first_create['date'] - first_commit['date']).total_seconds()
+
+def get_time_spend_on_pr_after_creation(events_with_time: list):
+    first_create = get_first_event_of_type(events_with_time, 'createAt')
+    first_merge = get_first_event_of_type(events_with_time, 'mergedAt')
+    return (first_merge['date'] - first_create['date']).total_seconds()
+
+def get_time_to_merge_after_last_review(event_with_time: list):
+    last_review = get_last_event_of_type(events_with_time, 'review')
+    first_merge = get_first_event_of_type(events_with_time, 'mergedAt')
+    return (first_merge['date'] - last_review['date']).total_seconds()
+    
+def get_time_spend_on_branch_until_merged(events_with_time: list):
+    time_spend_on_branch = 0
+    for i in range(len(events_with_time)):
+        time_spend_on_branch += events_with_time[i]['time']
+        if events_with_time[i]['type'] == 'mergedAt' or events_with_time[i]['type'] == 'closedAt':
+            break
+    return time_spend_on_branch
+
+def convert_seconds_to_a_readable_string(time_in_seconds: int):
+    time_in_seconds = int(time_in_seconds)
+    if time_in_seconds < 60:
+        return f'{time_in_seconds} seconds'
+    elif time_in_seconds < 3600:
+        return f'{time_in_seconds//60} minutes'
+    elif time_in_seconds < 86400:
+        return f'{time_in_seconds//3600} hours'
+    elif time_in_seconds < 604800:
+        return f'{time_in_seconds//86400} days'
+    elif time_in_seconds < 2629743:
+        return f'{time_in_seconds//604800} weeks'
+    else:
+        return f'{time_in_seconds//2629743} months'
+
+pr_id = int(sys.argv[1])
+
+result = get_detailed_pull_request_event_history(pr_id)
+# create object from json string result
+result = json.loads(result)
+# get the commits
+commits = result['commits']
+# get the reviews
+reviews = result['reviews']
+# get the state
+state = result['state'] 
+# get the comments
+comments = result['comments']
+# get the closedAt
+closedAt = result['closedAt']
+# get the createdAt
+createdAt = result['createdAt']
+# get the mergedAt
+mergedAt = result['mergedAt']
+
+# now, create a list of events
+events = []
+
+# merge all interesting events into a single list
+events.append([{'type': 'createAt', 'date': convert_to_datetime(createdAt), 'event_instance':createdAt}])
+events.append([{'type': 'commit', 'date': convert_to_datetime(commit['committedDate']), 'event_instance':commit} for commit in commits])
+events.append([{'type': 'review', 'date': convert_to_datetime(review['submittedAt']), 'event_instance':review} for review in reviews])
+events.append([{'type': 'mergedAt', 'date': convert_to_datetime(mergedAt), 'event_instance':mergedAt}])
+events.append([{'type': 'closedAt', 'date': convert_to_datetime(closedAt), 'event_instance':closedAt}])
+
+#flatten the list
+events = [item for sublist in events for item in sublist]
+
+# take care about events that don't have a valid date
+events = [event for event in events if event['date'] != None]
+# sort the events by date
+events.sort(key=lambda x: x['date'])
+
+
+
+# now, create a list of events with the time between events
+events_with_time = []
+
+
+
+
+
+for i in range(len(events)):
+    if i == 0:
+        events_with_time.append({'type': events[i]['type'], 'date': events[i]['date'], 'time': 0})
+    else:
+        events_with_time.append({'type': events[i]['type'], 'date': events[i]['date'], 'time': (events[i]['date'] - events[i-1]['date']).total_seconds()})
+
+
+time_spend_on_branch_before_pr_created = get_time_spend_on_branch_before_pr_created_from_timeline(events_with_time)
+time_spend_on_branch = get_time_spend_on_branch_until_merged(events_with_time)
+time_to_merge_after_last_review = get_time_to_merge_after_last_review(events_with_time)
+time_spend_on_pr_after_creation = get_time_spend_on_pr_after_creation(events_with_time)
+pr_comment_string = f'''
+## Pull Request Data
+|Description|Duration|
+|---|---|
+|Time that was spend on the branch before the PR was created|{convert_seconds_to_a_readable_string(time_spend_on_branch_before_pr_created)}|
+|Time that was spend on the branch before the PR was merged| {convert_seconds_to_a_readable_string(time_spend_on_branch)} |
+|Time to merge after the last review| {convert_seconds_to_a_readable_string(time_to_merge_after_last_review)} |
+|Time spend on the PR after creation| {convert_seconds_to_a_readable_string(time_spend_on_pr_after_creation)} |
+'''
+
+# write the pr_comment_string to a temporary file
+with open('pr_comment.txt', 'w') as file:
+    file.write(pr_comment_string)
