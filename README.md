c55938cdd4a6d67274c556f9f05b2e11b4e58e23
Beelmann, Marko <marko.beelmann@philips.com>
2023-02-08 00:05:14 +0100

    Created branch mbee20

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Created branch mbee18
+Created branch mbee20
--- create_pr.sh
+++ create_pr.sh
@@ -5,7 +5,9 @@ git commit -am "Created branch $1"
 git push --set-upstream origin $1
 gh pr create --title "Created branch $1" --body "Created branch $1" --base main --head $1
 gh pr comment "$1" --body "Created branch $1"
-#gh pr review $1 --comment -b "interesting"
+gh pr review $1 --comment -b "interesting"
 gh pr merge "$1" --squash --delete-branch
 git checkout main
 git pull
+
+# create a loop with a number that is part of the command that is echoed as one line command
